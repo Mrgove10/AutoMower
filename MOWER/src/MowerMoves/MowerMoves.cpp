9603475009fc48f060802019b705b0e6ae41f55e
Patrick <ppar666799@gmail.com>
2021-08-14 22:59:09 +0200

    Create a function to manage approaching objects and reduce speed

--- MOWER/src/MowerMoves/MowerMoves.cpp
+++ MOWER/src/MowerMoves/MowerMoves.cpp
@@ -103,6 +103,72 @@ void MowerReserseAndTurn(const int Angle, const int Duration, const bool OnSpot)
   MowerTurn(Angle, OnSpot);
 }
 
+/**
+ * Mower checks selected obstacle types and reduces speed if conditions are met
+ * @param SpeedDelta as int: the speed reduction to be applied expressed as a positive value (in absolue %). If multiple conditions are selected, same speed reduction is applied.
+ * @param Front as optional int: sonar measured front distance under which mower needs to slow down. 0 disbales the check. Default is 0
+ * @param Left as optional int: sonar measured left distance under which mower needs to slow down. 0 disbales the check. Default is 0
+ * @param Right as optional int: sonar measured right distance under which mower needs to slow down. 0 disbales the check. Default is 0
+ * @param Perimeter as optional int: perimeter wire signal magnitude under which  mower needs to slow down. 0 disables the check. Absolute value is used to perform the check (applies to both inside and outside perimeter wire).  Default is 0.
+ * @return boolean indicating if the function triggered a speed reduction
+ */
+bool MowerSlowDownApproachingObstables(const int SpeedDelta, const int Front, const int Left, const int Right, const int Perimeter)
+{
+  bool SpeedReductiontiggered = false;
+
+  // Check for objects in Front
+
+  if (Front > 0 && g_SonarDistance[SONAR_FRONT] < Front)
+  {
+      DebugPrintln("Front approaching object : Slowing down ! (" + String(g_SonarDistance[SONAR_FRONT]) + "cm)", DBG_DEBUG, true);
+      SpeedReductiontiggered = true;
+  } 
+
+  // Check for objects on left side
+
+  if (Left > 0 && g_SonarDistance[SONAR_LEFT] < Left)
+  {
+    DebugPrintln("Left approaching object : Slowing down ! (" + String(g_SonarDistance[SONAR_LEFT]) + "cm)", DBG_DEBUG, true);
+    SpeedReductiontiggered = true;
+  } 
+
+  // Check for objects on right side
+
+  if (Right > 0 && g_SonarDistance[SONAR_RIGHT] < Right)
+  {
+    DebugPrintln("Right approaching object : Slowing down ! (" + String(g_SonarDistance[SONAR_RIGHT]) + "cm)", DBG_DEBUG, true);
+    SpeedReductiontiggered = true;
+  } 
+
+  // Check for Perimeter wire
+
+  if (Perimeter > 0 && abs(g_PerimeterMagnitudeAvg) > Perimeter)
+  {
+    DebugPrintln("Approaching perimeter : Slowing down ! (" + String(g_PerimeterMagnitude) + ")", DBG_DEBUG, true);
+    SpeedReductiontiggered = true;
+  } 
+
+  // If at least one of the conditions are met and if motor speed is higher that minimum threshold, reduce speed
+
+  // Left Motor
+  if (SpeedReductiontiggered && g_MotionMotorSpeed[MOTION_MOTOR_LEFT] > MOTION_MOTOR_MIN_SPEED + SpeedDelta)
+  {
+    int newspeed = g_MotionMotorSpeed[MOTION_MOTOR_LEFT] - SpeedDelta;
+    DebugPrintln("Left motor speed reduced at " + String(newspeed) + "%", DBG_VERBOSE, true);
+    MotionMotorSetSpeed(MOTION_MOTOR_LEFT, newspeed);
+  }
+
+  // Right Motor
+  if (SpeedReductiontiggered && g_MotionMotorSpeed[MOTION_MOTOR_RIGHT] > MOTION_MOTOR_MIN_SPEED + SpeedDelta)
+  {
+    int newspeed = g_MotionMotorSpeed[MOTION_MOTOR_RIGHT] - SpeedDelta;
+    DebugPrintln("Right motor speed reduced at " + String(newspeed) + "%", DBG_VERBOSE, true);
+    MotionMotorSetSpeed(MOTION_MOTOR_RIGHT, newspeed);
+  }
+
+  return SpeedReductiontiggered;
+}
+
 /*
 void getMeUnstuck()
 {
--- MOWER/src/MowerMoves/MowerMoves.h
+++ MOWER/src/MowerMoves/MowerMoves.h
@@ -42,4 +42,15 @@ void MowerTurn(const int Angle, const bool OnSpot = false);
  */
 void MowerReserseAndTurn(const int Angle, const int Duration, const bool OnSpot = false);
 
+/**
+ * Mower checks selected obstacle types and reduces speed if conditions are met
+ * @param SpeedDelta as int: the speed reduction to be applied (in absolue %). If multiple conditions are selected, same speed reduction is applied.
+ * @param Front as optional int: sonar measured front distance under which mower needs to slow down. 0 disbales the check. Default is 0
+ * @param Left as optional int: sonar measured left distance under which mower needs to slow down. 0 disbales the check. Default is 0
+ * @param Right as optional int: sonar measured right distance under which mower needs to slow down. 0 disbales the check. Default is 0
+ * @param Perimeter as optional int: perimeter wire signal magnitude under which  mower needs to slow down. 0 disables the check. Absolute value is used to perform the check (applies to both inside and outside perimeter wire).  Default is 0.
+ * @return boolean indicating if the function triggered a speed reduction
+ */
+bool MowerSlowDownApproachingObstables(const int SpeedDelta, const int Front = 0, const int Left = 0, const int Right = 0, const int Perimeter = 0);
+
 #endif
