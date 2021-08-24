92f856257d3142de183b4975ec93de564db86ad5
Adrien RICHARD <richardadrien0@gmail.com>
2021-08-24 02:22:04 +0200

    added css to webpack

--- APP/src/index.js
+++ APP/src/index.js
@@ -1,5 +1,9 @@
 import { sendCommand } from './comm';
 
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "leaflet/dist/leaflet.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 window.start = function () {
     console.log('Sending Start');
     sendCommand('STATE_CHANGE','MOWING');
@@ -9,33 +13,33 @@ window.stop = function () {
     sendCommand('STATE_CHANGE','IDLE');
 }
 
-// window.setStateIcon = function (State) {
-//     switch (State) {
-//         case 'idle':
-//             return 'x';
-//             break;
-//         case 'docked':
-//             document.getElementById('stateDisplay').innerHTML = '<i class="fas fa - home"></i> Docked';
-//             break;
-//         case 'mowing':
-//             return 'x';
-//             break;
-//         case 'going_to_base':
-//             return 'x';
-//             break;
-//         case 'leaving_base':
-//             return 'x';
-//             break;
-//         case 'error':
-//             return 'x';
-//             break;
-//         case 'test':
-//             return 'x';
-//             break;
-//         default:
-//             document.getElementById('stateDisplay').innerHTML = "AAAA";
-//             break;
-//     }
-// }
+window.setStateIcon = function (State) {
+    switch (State) {
+        case 'idle':
+            return 'x';
+            break;
+        case 'docked':
+            document.getElementById('stateDisplay').innerHTML = '<i class="fas fa - home"></i> Docked';
+            break;
+        case 'mowing':
+            return 'x';
+            break;
+        case 'going_to_base':
+            return 'x';
+            break;
+        case 'leaving_base':
+            return 'x';
+            break;
+        case 'error':
+            return 'x';
+            break;
+        case 'test':
+            return 'x';
+            break;
+        default:
+            document.getElementById('stateDisplay').innerHTML = "AAAA";
+            break;
+    }
+}
 
-// console.log('Hello World!');
+console.log('Hello World!');
