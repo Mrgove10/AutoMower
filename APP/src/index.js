84bdc0489df3267d6d02ae661816b40d553bb576
Adrien RICHARD <richardadrien0@gmail.com>
2021-08-31 23:29:33 +0200

    better schedule

--- APP/src/index.js
+++ APP/src/index.js
@@ -1,5 +1,7 @@
 import { sendCommand } from './comm';
 
+let d = new Date()
+
 window.start = function () {
     console.log('Sending Start');
     sendCommand('STATE_CHANGE', 'MOWING');
@@ -9,52 +11,69 @@ window.stop = function () {
     sendCommand('STATE_CHANGE', 'IDLE');
 }
 
-window.setStateIcon = function (State) {
-    switch (State) {
-        case 'idle':
-            return 'x';
-            break;
-        case 'docked':
-            document.getElementById('stateDisplay').innerHTML = '<i class="fas fa - home"></i> Docked';
-            break;
-        case 'mowing':
-            return 'x';
-            break;
-        case 'going_to_base':
-            return 'x';
-            break;
-        case 'leaving_base':
-            return 'x';
-            break;
-        case 'error':
-            return 'x';
-            break;
-        case 'test':
-            return 'x';
-            break;
-        default:
-            document.getElementById('stateDisplay').innerHTML = "AAAA";
-            break;
-    }
-}
-
-console.log('Hello World!');
-
 const days = [
     "Monday",
     "Tuesday",
     "Wednesday",
     "Thursday",
     "Friday",
-    "Saterday",
+    "Saturday",
     "Sunday"
 ]
 
 const schedule = {
-    monday :{
-        start : "5:00",
-        end : '17:00'
-    }
+    "Monday": [
+        {
+            start: 5,
+            end: 12,
+            zone: "Alpha"
+        },
+        {
+            start: 14,
+            end: 17,
+            zone: "Beta"
+        },
+    ],
+    "Tuesday": [
+        {
+            start: 1,
+            end: 5,
+            zone: "Beta"
+        },
+        {
+            start: 6,
+            end: 18,
+            zone: "Alpha"
+        }
+    ],
+    "Wednesday": [
+        {
+            start: 7,
+            end: 19,
+            zone: "Beta"
+        }
+    ],
+    "Thursday": [
+        {
+            start: 8,
+            end: 20,
+            zone: "Beta"
+        }
+    ],
+    "Friday": [
+        {
+            start: 9,
+            end: 21,
+            zone: "Beta"
+        }
+    ],
+    "Sunday": [
+        {
+            start: 10,
+            end: 24,
+            zone: "Beta"
+        }
+    ]
 }
 
 // table header
@@ -66,21 +85,80 @@ days.forEach((day) => {
 for (let i = 1; i <= 24; i++) {
     let current = document.getElementById("tablebody").innerHTML
     let strDay = `<tr><td>${i}:00</td>`
-    if(i == new Date().getHours()){
-        console.log("current time");
-        days.forEach((day) => {
-            strDay = strDay + `<td class="has-events" rowspan="1">${day}</td>`
-        })
-    }
-    else{
-        
-        days.forEach((day) => {
-            strDay = strDay + `<td class="no-events" rowspan="1">${day}</td>`
-        })
-    }
-   
-
+    days.forEach((day) => {
+        strDay = strDay + `<td id="${i}_${day}" class="" rowspan="1"></td>`
+    })
     document.getElementById("tablebody").innerHTML = current + strDay + "</tr>";
 }
 
-//http://jsfiddle.net/dvirazulay/Lhe7C/
+// schedule coloring
+days.forEach((day) => {
+    if (schedule[day] !== undefined) {
+        console.log("shedule for " + day, schedule[day]);
+        for (let i = 0; i < 24; i++) {
+            var id = i + "_" + day;
+            schedule[day].forEach(timePeriode => {
+                if (i >= timePeriode.start && i <= timePeriode.end) {
+                    document.getElementById(id).innerHTML = timePeriode.zone
+                    if (timePeriode.zone === "Alpha") {
+                        document.getElementById(id).style = "background-color:cyan;";
+                    }
+                    else if (timePeriode.zone === "Beta") {
+                        document.getElementById(id).style = "background-color:green;";
+                    }
+                }
+            });
+        }
+    }
+    else {
+        console.log('no shedule for ' + day);
+    }
+})
+
+// outline the current day
+let dayNum = 0;
+days.forEach((day) => {
+    for (let i = 0; i < 24; i++) {
+        var id = i + "_" + day;
+        if (d.getHours() === i && d.getDay() === dayNum) {
+            console.log(d.getHours(), d.getDay());
+            document.getElementById(id).style = "outline:inset;"
+        }
+    }
+    dayNum++;
+})
+
+var mowerStates = {
+    "idle": {
+        humanName: "Idle",
+        icon: ""
+    },
+    "docked": {
+        humanName: "Docked",
+        icon: `<i class="fas fa - home"></i>`
+    },
+    "mowing": {
+        humanName: "Mowing",
+        icon: ""
+    },
+    "going_to_base": {
+        humanName: "Going to the dock",
+        icon: ""
+    },
+    "leaving_base": {
+        humanName: "Leaving dock",
+        icon: ""
+    },
+    "error": {
+        humanName: "Error",
+        icon: ""
+    },
+    "test": {
+        humanName: "test",
+        icon: ""
+    }
+}
+
+window.setStateIcon = function (State) {
+    document.getElementById('stateDisplay').innerHTML = `${mowerStates[State].icon} ${mowerStates[State].humanName}`
+}
