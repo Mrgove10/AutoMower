08415fce95e3eb2f6074af458e0510027e4cf7da
Patrick <ppar666799@gmail.com>
2021-09-09 20:33:51 +0200

    Battery SOC in decimals

--- MOWER/src/Battery/Battery.cpp
+++ MOWER/src/Battery/Battery.cpp
@@ -314,7 +314,7 @@ void BatteryChargeCheck(const bool Now)
 
     // Determine State of Charge
 
-    g_BatterySOC = (map(int(g_BatteryVoltage), int(BATTERY_0_PERCENT_VOLTAGE), int(BATTERY_VOLTAGE_FULL_THRESHOLD), 0, 100));
+    g_BatterySOC = (map(int(g_BatteryVoltage), int(BATTERY_0_PERCENT_VOLTAGE), int(BATTERY_VOLTAGE_FULL_THRESHOLD), 0, 1000))/10;
     
     if (g_BatteryVoltage <= BATTERY_0_PERCENT_VOLTAGE)
     { 
